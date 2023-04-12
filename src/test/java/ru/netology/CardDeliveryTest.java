f06c1f2fd7633b13ce5801c52e439d295b6eed22
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-04-12 22:05:55 +0400

    change format date tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
@@ -14,31 +13,37 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    String meetingDay(int day) {
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
-            public void openPage() {
+    public void openPage() {
         open("http://localhost:9999/");
     }
+
     static void setUp() {
         Configuration.headless = true;
+
     }
+
     @Test
     public void shouldCheckCorrectForm() {
         $x("//input [@placeholder='Город']").val("Астрахань");
         $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $x("//input[@placeholder='Дата встречи']").val(meetingDate);
+        $x("//input[@placeholder='Дата встречи']").val(meetingDay(3));
         $("[data-test-id='name'] input").setValue("Караулов Дмитрий");
         $("[data-test-id='phone'] input").setValue("+79033210166");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
-        $("[data-test-id='notification']").waitUntil(visible,15000);
+        $("[data-test-id='notification']").waitUntil(visible, 15000);
     }
+
     @Test
     void shouldCheckIncorrectCity() {
         $x("//input [@placeholder='Город']").val("Лондон");
         $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $x("//input[@placeholder='Дата встречи']").val(meetingDate);
+        $x("//input[@placeholder='Дата встречи']").val(meetingDay(3));
         $("[data-test-id='name'] input").val("Петров Иван");
         $("[data-test-id='phone'] input").val("+79253562244");
         $("[data-test-id='agreement']").click();
@@ -51,8 +56,7 @@ public class CardDeliveryTest {
     void shouldCheckNameEmpty() {
         $x("//input [@placeholder='Город']").val("Краснодар");
         $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $x("//input[@placeholder='Дата встречи']").val(meetingDate);
+        $x("//input[@placeholder='Дата встречи']").val(meetingDay(3));
         $("[data-test-id='name'] input").val(" ");
         $("[data-test-id='phone'] input").val("+79213332244");
         $("[data-test-id='agreement']").click();
